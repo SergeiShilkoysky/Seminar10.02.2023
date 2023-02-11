ed6ae5e08440fcdab003adb2757cef5a49a00207
Sergei Sh <sergshrb@mail.ru>
2023-02-11 23:30:49 +0300

    Вывод последней цифры 3-х значн. числа

--- Lessons5/Program.cs
+++ Lessons5/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает последнюю цифру этого числа.
+// 456->6  / 782->2 / 918 ->8
+
+Console.Clear();
+Console.Write("Вариант 1-й:  ");
+Console.Write("Введите трехзначное число: ");
+int num=int.Parse(Console.ReadLine());
+int y=num%10;
+
+Console.WriteLine($"{y}");
+
+
+Console.Write("Вариант 2-й:  ");
+Console.WriteLine("введите трехзначное число "); // n
+int n = Convert.ToInt32(Console.ReadLine());
+int d = n%10;
+
+Console.WriteLine(" " + d + " " );
