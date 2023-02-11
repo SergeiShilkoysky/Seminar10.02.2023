201fabb62dad9bdbea72a4043a190d149c19e93e
Sergei Sh <sergshrb@mail.ru>
2023-02-11 23:23:37 +0300

    Вывод квадрата числа

--- Lessons2/Program.cs
+++ Lessons2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя),
+// например, 4->16; -3->9; -7->49
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Способ1 Квадрат числа " + value + " равен: " + value * value);
+
+// способ 2 
+Console.WriteLine("Способ2  Квадрат числа " + value + " равен: " + Math.Pow(value, 2));
