d413f4237bd3ece0e96dcc076a61ca35ec2218ac
Sergei Sh <sergshrb@mail.ru>
2023-02-14 00:38:03 +0300

    max из трех  чисел

--- homework2/Program.cs
+++ homework2/Program.cs
@@ -1,4 +1,12 @@
 ﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 // 2, 3, 7 -> 7  /   44 5 78 -> 78   /  22 3 9 -> 22
 
+Console.Write("введите 1-е число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите 2-е число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите 3-е число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
 
+int max = Math.Max(Math.Max(numberA, numberB), Math.Max(numberB, numberC));
+Console.WriteLine( " max = " + max);
