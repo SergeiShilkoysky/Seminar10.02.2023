87a485de4911a51c2192f192dc11d6fb6fc4a81f
Sergei Sh <sergshrb@mail.ru>
2023-02-14 01:26:33 +0300

    четные от 1 до N

--- homework4/Program.cs
+++ homework4/Program.cs
@@ -4,6 +4,4 @@
 Console.Write("введите число:  ");
 int N = Convert.ToInt32(Console.ReadLine());
 for(int i = 2; i <= N; i=i+2)
-   {
-    Console.Write(i + ", " );
-   }
+   { Console.Write(i + ", "); }
